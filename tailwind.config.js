5d36ee523b2c0c80a46092b17497c7f2cd019bb7
Ron Kibel <ron.kibel1@gmail.com>
2024-06-26 18:30:18 -0700

    config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    `./src/pages/**/*.{js,jsx,ts,tsx}`,
+    `./src/components/**/*.{js,jsx,ts,tsx}`,
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'shiny-green': '#23c483',
+        'grad-upper': '#a6b2d1',
+        'grad-lower': '#d6f4ff',
+      },
+      strokeWidth: {
+        '1.5': '1.5px'
+      }
+    },
+  },
+  plugins: [],
+}
